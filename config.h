5c0495f93c259dcc0d42069148606cd4b0262593
Pavel Novichkov <9932831+PavelNovichkov@users.noreply.github.com>
2021-12-26 09:39:34 +0100

    Add Colemak layer

--- config.h
+++ config.h
@@ -3,3 +3,6 @@
 #define IGNORE_MOD_TAP_INTERRUPT // rollover without triggering mods
 #define TAPPING_FORCE_HOLD // no auto-repeat on double tap
 #define PERMISSIVE_HOLD
+
+#define COMBO_ONLY_FROM_LAYER 0
+#define COMBO_TERM 30
--- keymap.c
+++ keymap.c
@@ -1,18 +1,28 @@
 #include QMK_KEYBOARD_H
 #include "g/keymap_combo.h"
 
-#define BASE 0
-#define NUM 1
-#define SYMB 2
-#define NAV 3
-#define FN 4
-#define MOUSE 5
+#define QWERTY 0
+#define COLEMAK 1
+#define NUM 2
+#define SYMB 3
+#define NAV 4
+#define FN 5
+#define MOUSE 6
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-[BASE] = LAYOUT_split_3x5_3(
+[QWERTY] = LAYOUT_split_3x5_3(
     KC_Q, KC_W,         KC_E,         KC_R,         KC_T,            KC_Y,         KC_U,         KC_I,         KC_O,         KC_P,
     KC_A, LALT_T(KC_S), LCTL_T(KC_D), LSFT_T(KC_F), LGUI_T(KC_G),    RGUI_T(KC_H), RSFT_T(KC_J), RCTL_T(KC_K), LALT_T(KC_L), KC_SCLN,
     KC_Z, KC_X,         KC_C,         KC_V,         KC_B,            KC_N,         KC_M,         KC_COMM,      KC_DOT,       KC_SLSH,
+
+    KC_TAB,           LT(NAV, KC_SPC),  LT(MOUSE, KC_ESC),
+    LT(SYMB, KC_ENT), LT(NUM, KC_BSPC), LT(FN, KC_APP)
+    ),
+
+[COLEMAK] = LAYOUT_split_3x5_3(
+    KC_Q, KC_W,         KC_F,         KC_P,         KC_B,            KC_J,         KC_L,         KC_U,         KC_Y,         KC_SCLN,
+    KC_A, LALT_T(KC_R), LCTL_T(KC_S), LSFT_T(KC_T), LGUI_T(KC_G),    RGUI_T(KC_M), RSFT_T(KC_N), RCTL_T(KC_E), LALT_T(KC_I), KC_O,
+    KC_Z, KC_X,         KC_C,         KC_D,         KC_V,            KC_K,         KC_H,         KC_COMM,      KC_DOT,       KC_SLSH,
 
     KC_TAB,           LT(NAV, KC_SPC),  LT(MOUSE, KC_ESC),
     LT(SYMB, KC_ENT), LT(NUM, KC_BSPC), LT(FN, KC_APP)
