c05a3dcc9287b64c3086cc47b2ca4ee1d660e216
Pavel Novichkov <9932831+PavelNovichkov@users.noreply.github.com>
2022-01-02 18:15:03 +0100

    Redefine layers

--- keymap.c
+++ keymap.c
@@ -1,31 +1,20 @@
 #include QMK_KEYBOARD_H
 #include "g/keymap_combo.h"
 
-#define QWERTY 0
-#define COLEMAK 1
+#define BASE 0
+#define RUS 1 // TODO
 #define NUM 2
-#define SYMB 3
+#define FN 3
 #define NAV 4
-#define FN 5
-#define MOUSE 6
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-[QWERTY] = LAYOUT_split_3x5_3(
-    KC_Q, KC_W,         KC_E,         KC_R,         KC_T,            KC_Y,         KC_U,         KC_I,         KC_O,         KC_P,
-    KC_A, LALT_T(KC_S), LCTL_T(KC_D), LSFT_T(KC_F), LGUI_T(KC_G),    RGUI_T(KC_H), RSFT_T(KC_J), RCTL_T(KC_K), LALT_T(KC_L), KC_SCLN,
-    KC_Z, KC_X,         KC_C,         KC_V,         KC_B,            KC_N,         KC_M,         KC_COMM,      KC_DOT,       KC_SLSH,
+[BASE] = LAYOUT_split_3x5_3(
+    KC_Q, KC_W,         KC_F,         KC_P,         KC_B,    KC_J, KC_L,         KC_U,         KC_Y,         KC_QUOT,
+    KC_A, LALT_T(KC_R), LSFT_T(KC_S), LCTL_T(KC_T), KC_G,    KC_M, RCTL_T(KC_N), RSFT_T(KC_E), LALT_T(KC_I), KC_O,
+    KC_Z, KC_X,         KC_C,         LGUI_T(KC_D), KC_V,    KC_K, RGUI_T(KC_H), KC_COMM,      KC_DOT,       KC_SLSH,
 
-    KC_TAB,           LT(NAV, KC_SPC),  LT(MOUSE, KC_ESC),
-    LT(SYMB, KC_ENT), LT(NUM, KC_BSPC), LT(FN, KC_APP)
-    ),
-
-[COLEMAK] = LAYOUT_split_3x5_3(
-    KC_Q, KC_W,         KC_F,         KC_P,         KC_B,            KC_J,         KC_L,         KC_U,         KC_Y,         KC_SCLN,
-    KC_A, LALT_T(KC_R), LCTL_T(KC_S), LSFT_T(KC_T), LGUI_T(KC_G),    RGUI_T(KC_M), RSFT_T(KC_N), RCTL_T(KC_E), LALT_T(KC_I), KC_O,
-    KC_Z, KC_X,         KC_C,         KC_D,         KC_V,            KC_K,         KC_H,         KC_COMM,      KC_DOT,       KC_SLSH,
-
-    KC_TAB,           LT(NAV, KC_SPC),  LT(MOUSE, KC_ESC),
-    LT(SYMB, KC_ENT), LT(NUM, KC_BSPC), LT(FN, KC_APP)
+    KC_NO,  LT(NAV, KC_SPC), OSL(FN),
+    KC_APP, OSL(NUM),        KC_NO
     ),
 
 [NUM] = LAYOUT_split_3x5_3(
@@ -37,24 +26,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_NO,  KC_NO,   KC_NO
     ),
 
-[SYMB] = LAYOUT_split_3x5_3(
-    KC_GT,   KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN,    KC_NO,   KC_NO,   KC_NO,   KC_NO,   RESET,
-    KC_PLUS, KC_DLR,  KC_PERC, KC_CIRC, KC_LCBR,    KC_LGUI, KC_LSFT, KC_LCTL, KC_LALT, KC_NO,
-    KC_PIPE, KC_EXLM, KC_AT,   KC_HASH, KC_RCBR,    KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
-
-    KC_TILD, KC_UNDS, KC_DQUO,
-    KC_NO,   KC_NO,   KC_NO
-    ),
-
-[NAV] = LAYOUT_split_3x5_3(
-    KC_NO, KC_NO,   KC_NO,   KC_NO,   KC_NO,      KC_NO,   KC_HOME, KC_END,  KC_NO,   KC_NO,
-    KC_NO, KC_LALT, KC_LCTL, KC_LSFT, KC_LGUI,    KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_NO,
-    KC_NO, KC_NO,   KC_NO,   KC_NO,   KC_NO,      KC_NO,   KC_PGDN, KC_PGUP, KC_NO,   KC_NO,
-
-    KC_NO,  KC_NO,   KC_NO,
-    KC_NO,  KC_DEL,  KC_NO
-    ),
-
 [FN] = LAYOUT_split_3x5_3(
     KC_F12, KC_F7, KC_F8, KC_F9, KC_SLCK,    KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
     KC_F11, KC_F4, KC_F5, KC_F6, KC_PSCR,    KC_LGUI, KC_LSFT, KC_LCTL, KC_LALT, KC_NO,
@@ -64,12 +35,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_NO,   KC_NO,  KC_NO
     ),
 
-[MOUSE] = LAYOUT_split_3x5_3(
-    KC_NO, KC_NO,   KC_NO,   KC_NO,   KC_NO,      KC_NO, KC_WH_L, KC_MS_U, KC_WH_R, KC_NO,
-    KC_NO, KC_LALT, KC_LCTL, KC_LSFT, KC_LGUI,    KC_NO, KC_MS_L, KC_MS_D, KC_MS_R, KC_NO,
-    KC_NO, KC_NO,   KC_NO,   KC_NO,   KC_NO,      KC_NO, KC_WH_D, KC_WH_U, KC_NO,   KC_NO,
+[NAV] = LAYOUT_split_3x5_3(
+    KC_NO, KC_NO,   KC_NO,   KC_NO,   KC_NO,      KC_NO,   KC_HOME, KC_END,  KC_NO,   KC_NO,
+    KC_NO, KC_LALT, KC_LCTL, KC_LSFT, KC_LGUI,    KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_NO,
+    KC_NO, KC_NO,   KC_NO,   KC_NO,   KC_NO,      KC_NO,   KC_PGDN, KC_PGUP, KC_NO,   KC_NO,
 
-    KC_NO,   KC_NO,   KC_NO,
-    KC_BTN1, KC_BTN3, KC_BTN2
+    KC_NO,  KC_NO,   KC_NO,
+    KC_NO,  KC_DEL,  KC_NO
     )
 };
