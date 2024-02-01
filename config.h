3af22b20c28953bfe765c1fcd2335bd5e164477c
Pavel Novichkov <9932831+PavelNovichkov@users.noreply.github.com>
2024-02-01 15:51:23 +0100

    Update mod-tap settings

--- config.h
+++ config.h
@@ -3,8 +3,7 @@
 
 // Tap-hold
 #define TAPPING_TERM 200
-#define IGNORE_MOD_TAP_INTERRUPT // rollover without triggering mods
-#define TAPPING_FORCE_HOLD_PER_KEY // no auto-repeat on double tap (for some keys)
+#define QUICK_TAP_TERM 0 // no auto-repeat on double tap
 #define PERMISSIVE_HOLD
 
 #define COMBO_ONLY_FROM_LAYER 0
