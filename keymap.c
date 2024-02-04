9c8cd4e6dea6eca55712647caa05627074ffced2
Pavel Novichkov <9932831+PavelNovichkov@users.noreply.github.com>
2024-02-04 08:45:58 +0100

    Move key definitions to a separate file

--- keymap.c
+++ keymap.c
@@ -1,43 +1,5 @@
 #include QMK_KEYBOARD_H
-
-#define BASE 0
-#define SYM 1
-#define NUM 2
-#define NAV 3
-#define FN 4
-
-
-// Key definitions
-
-#define HOME_R LALT_T(KC_R)
-#define HOME_S LSFT_T(KC_S)
-#define HOME_T LCTL_T(KC_T)
-#define HOME_D LGUI_T(KC_D)
-#define HOME_N RCTL_T(KC_N)
-#define HOME_E RSFT_T(KC_E)
-#define HOME_I LALT_T(KC_I)
-#define HOME_H RGUI_T(KC_H)
-
-#define MY_ELPS RALT(KC_GRV)
-#define MY_NBSP RSA(KC_GRV)
-#define MY_EUR RALT(KC_4)
-#define MY_LSQT RALT(KC_9)
-#define MY_RSQT RALT(KC_0)
-#define MY_EN_D RALT(KC_MINS)
-#define MY_EM_D RSA(KC_MINS)
-#define MY_LDQT RALT(KC_LBRC)
-#define MY_RDQT RALT(KC_RBRC)
-#define MY_LGQT RSA(KC_LBRC)
-#define MY_RGQT RSA(KC_RBRC)
-
-
-// Custom keycodes
-
-enum custom_keycodes {
-  TG_RUS = SAFE_RANGE,
-  ESCAPE
-};
-
+#include "definitions.h"
 
 // Combos
 
